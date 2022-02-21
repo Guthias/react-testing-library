4c9d80fab61be40c8db479e5bf766d9a5a30bfab
guthias <gmgustavom@gmail.com>
2022-02-21 18:06:17 -0300

    creating area for About tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('About', () => {
+  it('Should contain a h2 with text \'About Pokédex\'', () => {
+    fail('empty test');
+  });
+
+  it('Should contain two paragraphs with texts about the pokedex', () => {
+    fail('empty test');
+  });
+
+  it('Should contain a pokedex image', () => {
+    fail('empty test');
+  });
+});
