1ddad67df41c0ff516dc00e53b6a44341b1c2c7a
guthias <gmgustavom@gmail.com>
2022-02-21 19:09:09 -0300

    Creating Area for Not Found tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/Notfound';
+
+describe('Not Found', () => {
+  it('Should contain a h2 with text \'Page requested not found 😭\'', () => { });
+
+  it('Should contain a image with pikachu crying', () => { });
+});
