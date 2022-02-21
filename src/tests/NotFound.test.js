8780c8f036934d8a9d0fbba21cee0c1ae47ce93e
guthias <gmgustavom@gmail.com>
2022-02-21 19:10:23 -0300

    test: rendering NotFound component before each test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,6 +3,12 @@ import { render, screen } from '@testing-library/react';
 import NotFound from '../components/Notfound';
 
 describe('Not Found', () => {
+  beforeEach(() => {
+    render(
+      <NotFound />,
+    );
+  });
+
   it('Should contain a h2 with text \'Page requested not found 😭\'', () => { });
 
   it('Should contain a image with pikachu crying', () => { });
