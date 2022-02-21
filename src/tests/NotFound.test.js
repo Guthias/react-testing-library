568b0286049f0924c89b00421641daf9d9897823
guthias <gmgustavom@gmail.com>
2022-02-21 19:21:37 -0300

    test: verifyng Not Found title

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import NotFound from '../components/Notfound';
+import NotFound from '../components/NotFound';
 
 describe('Not Found', () => {
   beforeEach(() => {
@@ -9,7 +9,11 @@ describe('Not Found', () => {
     );
   });
 
-  it('Should contain a h2 with text \'Page requested not found 😭\'', () => { });
+  it('Should contain a h2 with text \'Page requested not found\'', () => {
+    const titleElement = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(titleElement).toBeInTheDocument();
+  });
 
   it('Should contain a image with pikachu crying', () => { });
 });
