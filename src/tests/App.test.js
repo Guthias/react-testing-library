87f126dd17e76b74837fbbd0dfa0f1c199a09a87
guthias <gmgustavom@gmail.com>
2022-02-21 16:20:12 -0300

    test: testando se o link Home esta na aplicação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
 describe('Testando o componente App', () => {
   test('Teste se o link Home está presente na aplicação', () => {
-    //
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const linkElement = screen.getByRole('link', { name: 'Home' });
+    expect(linkElement).toBeInTheDocument();
   });
 });
