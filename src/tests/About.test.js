e08af8b5d5af5f7674acd5f25f6f25d738690aa8
guthias <gmgustavom@gmail.com>
2022-02-21 18:58:07 -0300

    removing unused imports from About test file

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import About from '../components/About';
 
 const imgUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
