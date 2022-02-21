0714d303e3da0b130be81ca23188a100de161bed
guthias <gmgustavom@gmail.com>
2022-02-21 17:29:04 -0300

    test: improving Home link test for also verify the url after click

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import App from '../App';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-describe('Testando o componente App', () => {
-  beforeEach(() => {
-    renderWithRouter(<App />);
-  });
-
-  test('Teste se o link Home está presente na aplicação', () => {
+describe('App', () => {
+  it('Should contain a Link with the text Home and redirect to \'/\'', () => {
+    const { history } = renderWithRouter(<App />);
     const linkElement = screen.getByRole('link', { name: 'Home' });
-    expect(linkElement).toBeInTheDocument();
+    userEvent.click(linkElement);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
 
   test('Teste se o link About está presente na aplicação', () => {
