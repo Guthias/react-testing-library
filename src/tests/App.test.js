46ff2ae49efdb6ac3b2bdfd2bfb36e9fed4c54b7
guthias <gmgustavom@gmail.com>
2022-02-21 16:22:19 -0300

    test: criando um beforeEach para os testes no App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,13 +4,15 @@ import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('Testando o componente App', () => {
-  test('Teste se o link Home está presente na aplicação', () => {
+  beforeEach(() => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
+  });
 
+  test('Teste se o link Home está presente na aplicação', () => {
     const linkElement = screen.getByRole('link', { name: 'Home' });
     expect(linkElement).toBeInTheDocument();
   });
