e909cd71a1f8e42d09d2575036c6aa846c6af972
guthias <gmgustavom@gmail.com>
2022-02-26 19:42:51 -0300

    creating FavoritePokemons test area

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Favorite Pokemmons', () => {
+  it('', () => {});
+});
