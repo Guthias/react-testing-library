b40bfbe230a48850009c03475d59164cce61442d
guthias <gmgustavom@gmail.com>
2022-02-25 21:09:58 -0300

    creating props object for test Pokemon component

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Pokemon from '../components/Pokemon';
 
+const props = {
+  pokemon: {
+    id: 393,
+    name: 'Piplup',
+    type: 'Water',
+    averageWeight: {
+      value: '5.2',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/a/a7/Spr_5b_393.png',
+  },
+  isFavorite: false,
+};
+
 describe('Pokemon', () => {
   beforeEach(() => {
     render(
-      <Pokemon />,
+      <Pokemon { ...props } />,
     );
   });
 
