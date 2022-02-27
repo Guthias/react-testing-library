aff1db643327b69a70616ed1ed64a404a329d1fb
guthias <gmgustavom@gmail.com>
2022-02-26 23:42:00 -0300

    creating props for test Pokedex component

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,5 +3,18 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
 
-describe('Pokedex', () => {});
+const isPokemonFavoriteById = {};
+pokemons.forEach(({ id }) => {
+  isPokemonFavoriteById[id] = false;
+});
+
+describe('Pokedex', () => {
+  it('Should contain a title with the text \'Encountered pokémons\'', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+  });
+});
