c67cec75d19de84e91a7312326c4a326060c4462
guthias <gmgustavom@gmail.com>
2022-02-26 22:35:17 -0300

    creating object for test Favorites screen

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,6 +4,39 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+  },
+  {
+    id: 148,
+    name: 'Dragonair',
+    type: 'Dragon',
+    averageWeight: {
+      value: '16.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png',
+  },
+];
+
 describe('Favorite Pokemmons', () => {
   it('Should have the text \'No favorite pokemon found\' when dont have a favorites',
     () => {
