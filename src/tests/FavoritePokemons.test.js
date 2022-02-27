5cfbba9427108dd35dc6267162e7b283f42e759d
guthias <gmgustavom@gmail.com>
2022-02-26 22:12:52 -0300

    test: testing if show right message when dont have favorite pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,5 +5,10 @@ import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Favorite Pokemmons', () => {
-  it('', () => {});
+  it('Should have the text \'No favorite pokemon found\' when dont have a favorites',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      const noFavoriteText = screen.getByText(/no favorite pokemon found/i);
+      expect(noFavoriteText).toBeInTheDocument();
+    });
 });
