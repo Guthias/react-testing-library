31061091fc7d52a9f16a4e5edc79f4f27f39c5a2
guthias <gmgustavom@gmail.com>
2022-02-27 20:07:09 -0300

    test: Verifying if Pokemon Details contain a title with pokemon name

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import PokemonDetails from '../components/PokemonDetails';
+import App from '../App';
 
 describe('Pokemon Details', () => {
-  it('Should contain ', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    console.log(history);
+    history.push('/pokemons/25');
+  });
 
+  it('Should contain a title with the text \'Pikachu details\'', () => {
+    const titleElement = screen.getByRole('heading', { name: /pikachu details/i });
+    expect(titleElement).toBeInTheDocument();
   });
 });
